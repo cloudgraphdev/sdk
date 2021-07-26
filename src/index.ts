aec79a1eebd110daf5527bcac35ead92ad9a35e1
tyler dunkel <tyler.dunkel@autocloud.dev>
2021-07-26 13:37:46 -0500

    feat(ProviderBase): add new provider base class to be extended. Simplified integration

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import logger, {Logger} from './logger'
+import Client from './client'
 import {Opts, Service, ServiceConnection} from './types'
 
-export type { Opts, Service, ServiceConnection, Logger }
+export type { Opts, Service, ServiceConnection, Logger, Client }
 export default {
-  logger
+  logger,
+  Client
 }
