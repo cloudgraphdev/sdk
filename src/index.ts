818836ef472b34466963fe94138e8917b7bc8b26
tyler dunkel <tyler.dunkel@autocloud.dev>
2021-08-19 17:16:16 -0500

    feat(interface): Create types for new provider interface, update Client abstract class, update scripts with build command

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import logger, {Logger} from './logger'
+import logger, { Logger } from './logger'
 import Client from './client'
-import {Opts, Service, ServiceConnection} from './types'
+import { Opts, Service, ServiceConnection, ProviderData } from './types'
 
-export type { Opts, Service, ServiceConnection, Logger, Client }
+export type { Opts, Service, ServiceConnection, Logger, Client, ProviderData }
 export default {
   logger,
-  Client
+  Client,
 }
