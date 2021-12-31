678add5f547bfa7972994c152b38e46a86425813
Marco Franceschi <marco.franceschi@autocloud.dev>
2021-12-31 15:49:30 -0300

    chore: Updated tests for rules engine

--- tests/rules-engine.test.ts
+++ tests/rules-engine.test.ts
@@ -7,7 +7,8 @@ const typenameToFieldMap = {
   resourceA: 'querySchemaA',
   resourceB: 'querySchemaB',
 }
-const schemaTypeName = 'providerFindings'
+const providerName = 'aws'
+const entityName = 'CIS'
 
 const ruleMock = {
   id: 'id',
@@ -30,7 +31,11 @@ const ruleMock = {
 describe('RulesEngine', () => {
   let rulesEngine: Engine
   beforeAll(() => {
-    rulesEngine = new RulesProvider('aws', 'CIS', typenameToFieldMap)
+    rulesEngine = new RulesProvider(
+      providerName,
+      entityName,
+      typenameToFieldMap
+    )
   })
   it('Should pass getting the updated schema created dynamically using schemaTypeName and typenameToFieldMap fields', () => {
     const schema = rulesEngine.getSchema()
@@ -71,11 +76,13 @@ describe('RulesEngine', () => {
     } = rulesEngine.prepareMutations(data)
 
     expect(findingsData).toBeDefined()
-    expect(findingsData.name).toBe(schemaTypeName)
+    expect(findingsData.name).toBe(`${providerName}${entityName}Findings`)
     expect(findingsData.mutation).toContain(typenameToFieldMap.resourceA)
     expect(findingsData.data.filter).toBeDefined()
     expect(findingsData.data.set).toBeDefined()
-    expect(findingsData.data.set.findings.length).toBe(data.length)
+    expect(findingsData.data.set[`${entityName}Findings`].length).toBe(
+      data.length
+    )
   })
 
   it('Should pass preparing the mutations to insert findings data given an empty RuleFindings array', () => {
