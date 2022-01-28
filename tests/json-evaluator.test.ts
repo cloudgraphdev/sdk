e18b6eab834f6efcadcc8ec80f44cd1aea6baea7
mariano.pizarro <mariano.pizarro@autocloud.dev>
2022-01-28 17:09:03 -0300

    feat: add daysDiff operator

--- tests/json-evaluator.test.ts
+++ tests/json-evaluator.test.ts
@@ -343,4 +343,28 @@ describe('JsonEvaluator', () => {
       await evaluator.evaluateSingleResource({ conditions: rule } as any, data)
     ).toBe(RuleResult.MATCHES)
   })
+
+  test('should calculate days diff between two date', async () => {
+    const day = 1000 * 60 * 60 * 24 // @TODO - replace by some date library (that is not moment)
+    const now = Date.now()
+    const data = {
+      data: {
+        cryptoKeys: [{ nextRotationTime: new Date(now + 90 * day).toISOString() }],
+      },
+    } as any
+    const rule: any = {
+      value: { path: 'cryptoKeys[0].nextRotationTime', daysDiff: {} },
+      lessThanInclusive: 90,
+    }
+
+    rule.lessThanInclusive = 80
+    expect(
+      await evaluator.evaluateSingleResource({ conditions: rule } as any, data)
+    ).toBe(RuleResult.DOESNT_MATCH)
+
+    rule.lessThanInclusive = 90
+    expect(
+      await evaluator.evaluateSingleResource({ conditions: rule } as any, data)
+    ).toBe(RuleResult.MATCHES)
+  })
 })
