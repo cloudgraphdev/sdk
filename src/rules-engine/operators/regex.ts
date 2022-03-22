b22971c1425a24d41ce15f9f8f2cf9a0fddf7726
Marco Franceschi <marco.franceschi@autocloud.dev>
2022-03-22 11:25:09 -0300

    feat: Included matchAny for regex operators

--- src/rules-engine/operators/regex.ts
+++ src/rules-engine/operators/regex.ts
@@ -2,4 +2,16 @@
 export default {
   match: (a, b) => new RegExp(b).test(a),
   mismatch: (a, b) => !new RegExp(b).test(a),
+  matchAny: (a, b) => {
+    if (Array.isArray(a)) {
+      const result = a
+        .map(value =>
+          typeof value === 'string' ? !new RegExp(b).test(value) : undefined
+        )
+        .some(v => v)
+      return result
+    }
+
+    return false
+  },
 }
