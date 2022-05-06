c1d6fa3d2dec99bc25d9454b0c7801340650cffe
Marco Franceschi <franceschi.marco@gmail.com>
2022-05-05 20:18:53 -0400

    feat: Adding utils to generate mutations dynamically

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ import {
   ServiceConnection,
   ProviderData,
   Entity,
+  EntityMutations,
   SchemaMap,
 } from './types'
 import {
@@ -41,6 +42,7 @@ import {
   mergeSchemas,
   getSchemaFromFolder,
   generateSchemaMapDynamically,
+  generateEntityMutations,
 } from './utils/schema'
 
 // Export Utils
@@ -52,6 +54,7 @@ export {
   mergeSchemas,
   getSchemaFromFolder,
   generateSchemaMapDynamically,
+  generateEntityMutations,
 }
 
 export { PluginModule, PluginType, Result, pluginMap }
@@ -69,6 +72,7 @@ export type {
   JsRule,
   JsonRule,
   Entity,
+  EntityMutations,
   StorageEngineConnectionConfig,
   StorageEngineConfig,
   StorageEngine,
