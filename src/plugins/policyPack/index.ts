3b16dabe7a79cd3e019798da9e3e8f69dd278712
mariano.pizarro <mariano.pizarro@autocloud.dev>
2022-06-29 12:05:27 -0300

    feat: adapt CLI to pass data instead of queries

--- src/plugins/policyPack/index.ts
+++ src/plugins/policyPack/index.ts
@@ -137,10 +137,12 @@ export default class PolicyPackPlugin extends Plugin {
   }
 
   private async executeRule({
+    data,
     rules,
     policyPack,
     storageEngine,
   }: {
+    data: any
     rules: Rule[]
     policyPack: string
     storageEngine: StorageEngine
@@ -159,18 +161,18 @@ export default class PolicyPackPlugin extends Plugin {
 
           findings.push(
             ...(await this.executeRule({
+              data,
               rules: subRules,
               policyPack,
               storageEngine,
             }))
           )
         } else {
-          const { data } = rule.gql
-            ? await storageEngine.query(rule.gql)
-            : { data: undefined }
+          const ruleData = rule.gql ? data : undefined
+
           const results = (await this.policyPacksPlugins[
             policyPack
-          ]?.engine?.processRule(rule, data)) as RuleFinding[]
+          ]?.engine?.processRule(rule, ruleData)) as RuleFinding[]
 
           findings.push(...results)
         }
@@ -200,6 +202,60 @@ export default class PolicyPackPlugin extends Plugin {
     const dataProcessor = new DgraphDataProcessor(config)
     this.dataProcessors[dataProcessorKey] = dataProcessor
     return dataProcessor
+  }
+
+  private getLinkedData({ providerData }: { providerData: ProviderData }): any {
+    const linkedData = {}
+    const allEntities = providerData?.entities || []
+    const allConnections = providerData?.connections || {}
+    const entitiesById: { [key: string]: any } = {}
+
+    for (const entity of allEntities) {
+      // AddawsEc2Input! => queryawsEc2
+      const mutationName = /(?<=\[)(.*?)(?=\])/
+        .exec(entity.mutation as any)[0]
+        .replace('Add', 'query')
+        .replace('Input!', '')
+
+      linkedData[mutationName] = entity.data
+
+      for (const entityData of entity.data) {
+        entitiesById[entityData.id] = entityData
+        // eslint-disable-next-line no-underscore-dangle
+        entityData.__typename = mutationName.replace('query', '') // or entity.name?
+      }
+    }
+
+    // connect data on a second pass
+    for (const entityId of Object.keys(allConnections)) {
+      const entityConnections = allConnections[entityId]
+      const entity = entitiesById[entityId]
+      if (!entity) {
+        // eslint-disable-next-line no-continue
+        continue
+      }
+      for (const conn of entityConnections) {
+        const targetEntity = entitiesById[conn.id]
+        if (!targetEntity) {
+          // eslint-disable-next-line no-continue
+          continue
+        }
+        if (conn.relation === 'child') {
+          if (!entity[conn.field]) {
+            entity[conn.field] = []
+          }
+          entity[conn.field].push(targetEntity)
+          // inverse relation
+          // const inverseConnField = this.schemasMap[entity.__typename] || 'account' // @TODO: account doesn't have a name
+          // if (!targetEntity[inverseConnField]) {
+          //   targetEntity[inverseConnField] = []
+          // }
+          // targetEntity[inverseConnField].push(entity)
+        } // else parent relation.. is not used atm
+      }
+    }
+
+    return linkedData
   }
 
   async configure(
@@ -277,10 +333,12 @@ export default class PolicyPackPlugin extends Plugin {
   async execute({
     storageRunning,
     storageEngine,
+    providerData,
     processConnectionsBetweenEntities,
   }: {
     storageRunning: boolean
     storageEngine: StorageEngine
+    providerData: ProviderData
     processConnectionsBetweenEntities: (props: {
       provider?: string
       providerData: ProviderData
@@ -314,7 +372,10 @@ export default class PolicyPackPlugin extends Plugin {
           mergeSchemas(currentSchema, findingsSchema),
         ])
 
+        const linkedData = this.getLinkedData({ providerData })
+
         const findings = await this.executeRule({
+          data: linkedData,
           rules,
           policyPack,
           storageEngine,
